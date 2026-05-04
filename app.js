72aa699f12581e6d39deb840ab8fa51bcb84367e
Émanuel Merveille <m.merveille.emanuel@gmail.com>
2026-05-03 21:06:45 -0400

    New commit message

--- app.js
+++ app.js
@@ -302,7 +302,7 @@ const addToSnipcart = () => {
     quantity: formState.quantity,
     image: product.images[0],
     description: product.description[0],
-    url: 'https://luxuryrawhair.netlify.app/',
+    url: 'https://luxuryrawhair.com/',
     customFields: [
       { 
         name: "Texture",
